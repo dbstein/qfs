8ed1832655690e92c1f6c90219d751c60089d5ce
David Stein <dstein@flatironinstitute.org>
2022-02-02 12:59:42 -0500

    updated license and setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='qfs',
-    version='0.0.1',
+    version='0.2',
     description='Quadrature by fundamental solutions',
     long_description=long_description,
     url='https://github.com/dbstein/pykifmm2d/',
